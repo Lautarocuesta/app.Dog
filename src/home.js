c36223dd618df076eba3082901c9edcbc07caf11
Paul Josué Michel <d47303647@instituto.itr.edu.ar>
2023-09-13 11:27:14 -0300

    form algo

--- src/home.js
+++ src/home.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import axios from "axios";
 import { useState } from "react";
+import Dogcard from "./dogCard";
+import "./home.css"
+
 
 function Home() {
+  const [ignore, setIgnore] = useState({
+    "ignore_energy": false,
+    "ignore_protec": false,
+    "ignore_train": false
+  });
   const [dogs, setDogs] = useState([]);
   const [formData, setFormData] = useState({
     size: "",
-    shedding: false,
+    shedding: "noImporta",
     energy: 0,
     protectiveness: 0,
     trainability: 0
@@ -15,6 +23,10 @@ function Home() {
   const handleChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
+
+  const ocult = (event) => {
+    setIgnore({ ...ignore, [event.target.name]: event.target.value });
+  }
 
   const getDogs = (event) => {
     event.preventDefault();
@@ -37,11 +49,8 @@ function Home() {
         min_height = 20;
         max_height = 35;
         break;
-
     }
 
-
-    
 
     axios.get("https://api.api-ninjas.com/v1/dogs", {
       headers: {'X-Api-Key': 'OvGYJdgkt/MnEjd5LZsqpQ==NYJvmG1NRqGTTWRl'},    //no nos baneen
@@ -49,7 +58,7 @@ function Home() {
         min_height: min_height,
         max_height: max_height,
         //barking: 5,
-        energy: 4,
+        energy: dogs.energy,
         //protectiveness: 5,
         //trainability: 5,
         //price: $1
@@ -57,8 +66,8 @@ function Home() {
     })
       .then((response) => setDogs(response.data))
       .catch((error) => console.error(`Error: ${error}`));    
-      
-      
+
+
       if(formData.shedding == "importa"){
         setDogs(dogs.filter((e) => e.shedding < 3))
       } 
@@ -70,37 +79,39 @@ function Home() {
         <h1 className="title">Encuentra a tu perro ideal</h1><br />
         <form className="dog-form" onSubmit={getDogs}>
           <label htmlFor="size">Tamaño: </label>
-          <input type="range" max="2" id="size" name="size" onChange={handleChange}/>
+          <select onChange={handleChange}>
+              <option value="0">Pequeño</option>
+              <option value="1">Mediano</option>
+              <option value="2">Grande</option>
+          </select>
           <br />
           <label htmlFor="shedding">Cuanto pelo arroja la raza:</label>
-          
-          <input type="radio" value="importa" id="shedding1" name="shedding" onChange={handleChange}/><label>suelta poco pelo</label>
-          <input type="radio" value="noImporta" id="shedding2" name="shedding" onChange={handleChange}/><label>no me importa</label>
+
+          <div className="radios">
+            <input type="radio" value="importa" className="shedding" onChange={handleChange}/><label>Suelta poco pelo</label>
+            <input type="radio" value="noImporta" className="shedding" onChange={handleChange}/><label>No me importa</label>
+          </div>
           <br />
 
-          <input type="checkbox" value="ignorar" name="ignorar" onChange={handleChange} />
           <label htmlFor="energy">Cuanta energia tiene la raza:</label>
-          <input type="range" max="5" min="1" id="energy" name="energy" onChange={handleChange}/>
+          <input type="checkbox" name="ignore_energy" onChange={ocult} />
+          {!ignore.ignore_energy && <input type="range" max="5" min="1" id="energy" name="energy" onChange={handleChange}/>}
           <br />
 
-          <input type="checkbox" value="ignorar" name="ignorar" onChange={handleChange} />
-          <label htmlFor="protectiveness">Que tan guardian es la raza:</label><br />
-          <input type="range" max="5" min="1" id="protectiveness" name="protectiveness" onChange={handleChange}/>
+          <label htmlFor="protectiveness">Que tan guardian es la raza:</label>
+          <input type="checkbox" name="ignore_protec" onChange={ocult} />
+          {!ignore.ignore_protec && <input type="range" max="5" min="1" id="protectiveness" name="protectiveness" onChange={handleChange}/>}
           <br />
 
-          <input type="checkbox" value="ignorar" name="ignorar" onChange={handleChange} />
           <label htmlFor="trainability">Que tan entrenable es la raza:</label>
-          <input type="range" max="5" min="1" id="trainability" name="trainability" onChange={handleChange}/>
+          <input type="checkbox" name="ignore_train" onChange={ocult} />
+          {!ignore.ignore_train && <input type="range" max="5" min="1" id="trainability" name="trainability" onChange={handleChange}/>}
           <br />
 
           <button type="submit">Buscar Perro</button>
         </form>
       </header>
-      <div>
-        <ul className="listDogs">
-          {dogs.map((p) => <li><img src={p.image_link}/><p>{p.name}</p></li>)}
-        </ul>
-      </div>
+          <Dogcard dogs={dogs}/>
     </div>
   );
 }
